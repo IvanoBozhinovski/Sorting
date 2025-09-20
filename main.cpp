5997091f8f46f9e6e50b964dca99e569a376957c
IvanoBozhinovski <ivano.bozhinovski@gmail.com>
2025-09-20 17:55:39 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,17 +4,36 @@
 
 using namespace std;
 
-void selection_sort(int* a,int n)
+int part(int* a, int l, int r)
 {
-    for(int i=0;i<n;i++)
+    //int p=a[l];
+
+    int p=a[(l+r)/2];  ///druga implementacija
+
+    int i=l-1,j=r+1;
+    while(true)
     {
-        int idx=i;
-        for(int j=i+1;j<n;j++)
+        do
         {
-            if(a[j]<a[idx])idx=j;
+            i++;
         }
-        swap(a[idx],a[i]);
+        while(a[i]<p);
+        do
+        {
+            j--;
+        }
+        while(a[j]>p);
+        if(i>=j)return j;
+        swap(a[i],a[j]);
     }
+}
+
+void quick_sort(int* a,int l,int r)
+{
+    if(l>=r)return;
+    int p=part(a,l,r);
+    quick_sort(a,l,p);
+    quick_sort(a,p+1,r);
 }
 
 int main()
@@ -32,11 +51,11 @@ int main()
 
     auto t1=chrono::steady_clock::now();
 
-    selection_sort(a,n);
+    quick_sort(a,0,n-1);
 
     auto t2=chrono::steady_clock::now();
 
-    double t=chrono::duration_cast<chrono::milliseconds>(t2-t1).count();
+    int t=chrono::duration_cast<chrono::milliseconds>(t2-t1).count();
     cout<<"Potrebni bea "<<t<<" milisekundi."<<endl;
 
     for(int i=1;i<n;i++)
@@ -50,9 +69,9 @@ int main()
     cout<<"PODREDENO"<<endl;
 
     /*int b[n];
-    for(int i=0;i<n;i++)b[i]=n-i;
+    for(int i=0;i<n;i++)b[i]=i;
     t1=chrono::steady_clock::now();
-    selection_sort(b,n);
+    quick_sort(b,0,n-1);
     t2=chrono::steady_clock::now();
     t=chrono::duration_cast<chrono::milliseconds>(t2-t1).count();
     cout<<"Potrebni bea "<<t<<" milisekundi."<<endl;*/
